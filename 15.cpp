cc49452dfaadd5b0b489764a694d7f8b8c97c551
Nauris <User>
2023-07-10 09:31:29 +0300

    simplify

--- 15.cpp
+++ 15.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-#include <cmath>
 #include <cstdint>
 #include <vector>
 
 using namespace std;
 
-
 /*
-	P = (2n)! / (n! * n!) = (2n)! /(n!)^2
+	P = (2n)! / (n! * n!) = Prod(k = n + 1 .. 2n, k) / n!
 */
 
 int64_t fact(int64_t i) { return i * ((i == 1) ? 1 : fact(i - 1)); }
@@ -21,18 +19,19 @@ int main(int c, char **v) {
 	vector<int> muls;
 	vector<int> divs;
 	int64_t r = 1;
-	for (int i = 1; i <= 2 * limit; i++) {
-		muls.push_back(i);
-		if (i <= limit)
-			divs.push_back(i * i);
+	for (int i = 1; i <= limit; i++) {
+		muls.push_back(i + limit);
+		divs.push_back(i);
 	}
-	for (int i = 0; i < muls.size(); i++) {
-		r *= muls[i];
-		for (int j = 0; j < divs.size(); j++) {
-			if (divs[j] > 1 && divs[j] <= r && !(r % divs[j])) {
-				r /= divs[j];
-				divs[j] = 1;
+	for (auto &m: muls) {
+		r *= m;
+		for (int i = 0; i <= divs.size();) {
+			if (divs[i] > 1 && divs[i] <= r && !(r % divs[i])) {
+				r /= divs[i];
+				divs.erase(divs.begin() + i);
+				continue;
 			}
+			i++;
 		}
 	}
 	cout << r << endl;
