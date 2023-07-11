d1f341b3fef4b11e59505ea94c821fb150d29699
Nauris Brunins <naurisb@mikrotik.com>
2023-07-11 10:53:38 +0300

    remove spaces

--- 16.cpp
+++ 16.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 string powerUp(string in) {
 	string out;
-	int c = 0;	
+	int c = 0;
 	for (int i = 0; i < in.size(); i++) {
 		int v = (in[i] - '0') * 2 + c;
 		c = v / 10;
@@ -18,12 +18,12 @@ string powerUp(string in) {
 
 int main() {
 	int r = 0, limit = 1000;
-	string s = "1";	
+	string s = "1";
 	for (int i = 0; i < limit; i++) {
 		s = powerUp(s);
 //		cout << s << endl;
 	}
-	
+
 	for (int i = 0; i < s.size(); i++)
 		r += s[i] - '0';
 	cout << r << endl;
