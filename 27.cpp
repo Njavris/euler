6435a220d76ad8d613902ba895fe85267cc6aad8
Nauris Brunins <naurisb@mikrotik.com>
2023-07-18 15:15:24 +0300

    fix logic mistake

--- 27.cpp
+++ 27.cpp
@@ -9,9 +9,9 @@ using namespace std;
 	-1000 <= b <= 1000
 	a, b - primes
 
-	topmost limits:
-	a = b = n:
-		n*n + n*n + n = n(2n + 1)
+	edgecases:
+	n = 0:
+		p = b => b - prime and b >= 2;
 */
 
 int main() {
@@ -30,28 +30,21 @@ int main() {
 			pr.push_back(i);
 	}
 
-	cout << pr.size() << endl;
 	int best_n = -1;
 	int best = -1;
-	int signs[][2] = { { 1, 1 }, { -1, 1 }, { 1, -1 }, { -1, -1 }};
-	for (auto &a: pr) {
-		if (a >= 1000)
-			break;
+	int signs[] = { 1, -1 };
+	for (int a = 0; a < 1000; a++) {
 		for (auto &b: pr) {
 			if (b > 1000)
 				break;
 			for (auto &s: signs) {
 				int n = 0;
 				while (true) {
-					int val = n * (n + s[0] * a) + s[1] * b;
+					int val = n * (n + s * a) + b;
 					bool found = 0;
 					for (auto &p: pr) {
-						if (val < p)
+						if ((found = (val == p)) || (val < p))
 							break;
-						if (val == p) {
-							found = true;
-							break;
-						}
 					}
 					if (!found)
 						break;
@@ -59,13 +52,12 @@ int main() {
 				}
 				if (n > best_n) {
 					best_n = n;
-					best = s[0] * s[1] * a * b;
-					cout << s[0] * a << " " << s[1] * b << " " << n << endl;;
+					best = s * a * b;
 				}
 			}
 		}
 	}
-	cout << best_n << " " << best << endl;
+	cout << best << endl;
 
 	return 0;
 }
